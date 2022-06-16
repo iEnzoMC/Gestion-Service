2a7a72fe49d8624741edbac1ce6d281b2ec87ea2
Enzo <segundacuenta_1@hotmail.com>
2022-06-16 20:22:01 -0300

    cambios

--- src/Pages/Configmenu.js
+++ src/Pages/Configmenu.js
@@ -3,10 +3,11 @@ import 'animate.css';
 import AddIcon from '@mui/icons-material/Add';
 import Fab from '@mui/material/Fab';
 import Button from '@mui/material/Button';
-import Switch from '@mui/material/Switch';
 import { LSConnection } from '../helper/LSConnection';
-import TextField from '@mui/material/TextField';
 import Casos from '../helper/Casos';
+import Box from '@mui/material/Box';
+import Switch from '@mui/material/Switch';
+import TextField from '@mui/material/TextField';
 
 function Configmenu({setError, error, setErrorMessage, errorMessage}) {
 
@@ -32,7 +33,6 @@ function Configmenu({setError, error, setErrorMessage, errorMessage}) {
     }, [names])
 
     const handleAddName = e =>{
-        e.preventDefault();
         let id = new Date().getTime()
         if(names !== false){
             setNames(element =>{
@@ -112,87 +112,61 @@ function Configmenu({setError, error, setErrorMessage, errorMessage}) {
   return (
 
 
-    <div className='fondoConfigMenu contenerConfigMenu'>
-        <form className='formularioConfigMenu'>
+    <form className='formulario-menu'>
 
-            <label>Nombre</label>
-            <div className='divisionConfigMenu'>
-                <div>
-                    <input className='hoverConfigMenu' type='text' placeholder='E.j: Pizza' name='name' />
-                    <Fab color="primary" aria-label="add" className='iconaddConfigMenu'>
-                        <AddIcon onClick={e => handleAddName(e)}/>
-                    </Fab>
-                </div>               
-                {names !== false &&
-               
-                names.map((element, i) =>{
-                    return(
-                    <div className='testt' key={element}>
-                        <input className='hoverConfigMenu' type='text' placeholder={"E.j Pizza "+(i+1)} name={"name"+i} />
-                        <Button name={element} onClick={e => handleRemoveName(e)} variant="outlined" color="error" className='botonEliminarConfigMenu'>
-                            Eliminar
-                        </Button>
-                    </div>
-                    )         
-                })
-                
-                
-                }
-                
-             </div>
+        <div className='nombre-y-espesificacion'>
+        
+            <label>Nombre(*)</label>
+            <input type='text' placeholder='E.j: Muzza' name='nombre'/>
+            <Fab color="primary" aria-label="add">
+            <AddIcon />
+            </Fab>
 
-            <div className='divisionConfigMenu'>
-             <label>Espesificaciones</label>
-             <textarea className='hoverConfigMenu' type='text' placeholder='Muzarela 8 porciones'  name='info'/>
+            <label>Espesificacion(*)</label>
+            <textarea type='text' placeholder='E.j: Muzza de 10 porciones' name='info'/>
+        
+        </div>
+        
+        <div className='crear-y-agregar'>
+
+            <label>Crear / Agregar Categoria(*)</label>
+
+            <div className='crear-y-agregar-unirInput'>
+                <TextField id="standard-basic" variant="standard" />     
+                <Button style={{marginLeft: '5px', width: '20px'}} variant="contained" color="success">
+                    Crear
+                </Button>
             </div>
 
-            <div className='divisionConfigMenu'>
-                <label>Crear / Agregar Categoria</label>
-                
-                <div onSubmit={e => handleCrearCategoria(e)} id='tasd'>
-                    <TextField id="standard-basic" label="Crear" variant="standard" onChange={e => handleCategory(e)} />
-                    <Button variant="outlined" color="success" className='botonCrearConfigMenu' onClick={e => handleCrearCategoria()}>
-                        CREAR
-                    </Button>
-                </div>
-
-
-                
-                    {Array.isArray(categorias) &&
-                    categorias.map(element =>{
-                        return(
-                            <div key={element.name} className='categorias-divisionConfigMenu'>
-                                <span>{element.name}</span>
-                                <Casos
-                                    objeto={element}
-                                    setNames={setNames}
-                                    names={names}
-                                />
-                                <Button name={element.name} onClick={e => handleRemoveCategory(e)} variant="outlined" color="error" className='botonEliminarConfigMenu'>
-                                    Eliminar
-                                </Button>
-                            </div>
-                        )
-                    })
-                 }
-                
-
+            <div className='crear-y-agregar-unirPromocion'>
+                <span style={{display: 'block', marginTop: '5px'}}>Promocion</span>
+                <Switch
+                    checked={checked}
+                    onChange={handleChange}
+                    inputProps={{ 'aria-label': 'controlled' }}
+                />
             </div>
+            
 
-            <div className='divisionConfigMenu'>
-                <label>Precio Final</label>
-                <input className='hoverConfigMenu' type='number' placeholder='50$'  name='price'/>
-            </div>
-
-            <input type="submit" value='Agregar' />
+            <label style={{display: 'block', marginTop: '10px'}}>Contacto</label>
+            <input type='number' placeholder='E.j: 3834213594' name='celular'/>
 
 
+        </div>
 
-        </form>
-    </div>
+        <div className='precio-final'>
+            <label>Precio Final</label>
+            <input type='number' placeholder='E.j: 50' name='celular'/>
+            <input type='submit' value='GUARDAR'/>
+        </div>
+ 
+
+
+    </form>
 
 
   );
 }
+
 
 export default Configmenu;
