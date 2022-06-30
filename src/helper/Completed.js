01d46998bdf7d116b4ed53e9cd08e54bb4e0738d
Enzo <segundacuenta_1@hotmail.com>
2022-06-30 15:56:20 -0300

    Ambios xd

---
+++ src/helper/Completed.js
@@ -0,0 +1,37 @@
+import * as React from 'react';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+import Snackbar from '@mui/material/Snackbar';
+import MuiAlert from '@mui/material/Alert';
+
+const Alert = React.forwardRef(function Alert(props, ref) {
+  return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+});
+
+
+export const Completed = ({setCompleted, completed, setCompletedMessage, completedMessage}) => {
+
+  
+    const handleClose = (event, reason) => {
+      if (reason === 'clickaway') {
+        return;
+      }
+  
+      setCompleted(false);
+
+      
+
+    };
+
+    let message = completedMessage
+
+  return (
+    <Stack spacing={2} sx={{ width: '100%' }}>
+      <Snackbar open={completed} autoHideDuration={6000} onClose={handleClose}>
+        <Alert onClose={handleClose} severity="success" sx={{ width: '100%' }}>
+          {message}
+        </Alert>
+      </Snackbar>
+    </Stack>
+  )
+}
